90e0adc9d745fa17a9d7819e14161e1307eb7a47
jayas0001Kanhaiya <kanhaiya.singh@ahom.tech>
2023-11-18 10:40:27 +0530

    landing-page-ui-bugs-fixes

--- src/components/table/CryptoTable.js
+++ src/components/table/CryptoTable.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
+import { Button } from "../button/Button";
 import "./cryptoTable.css";
 const navigation = ["Spot", "Future", "Gainers", "New", "24th Vol"];
 export const CryptoStatsTable = ({ data }) => {
+  const [activeNav, setActiveNav] = useState(navigation[0]);
   return (
     <>
       <div className="crypto-table-container">
@@ -9,7 +12,20 @@ export const CryptoStatsTable = ({ data }) => {
             <nav>
               <ul className="table-nav">
                 {navigation.map((item, index) => {
-                  return <li key={index}>{item}</li>;
+                  return (
+                    <li
+                      key={index}
+                      onClick={() => setActiveNav(item)}
+                      className={
+                        item === activeNav
+                          ? "table-nav-active"
+                          : "table-nav-inactive"
+                      }
+                      style={{cursor: "pointer"}}
+                    >
+                      {item}
+                    </li>
+                  );
                 })}
               </ul>
             </nav>
@@ -29,8 +45,12 @@ export const CryptoStatsTable = ({ data }) => {
                     <td className="crypto-name">{item.name}</td>
                     <td className="crypto-price">{item.price}</td>
                     <td className="crypto-change">{item.change}</td>
-                    <td className="crypto-market-stats">{item.marketStats}</td>
-                    <td className="crypto-action">{item.action}</td>
+                    <td className="crypto-market-stats">
+                      <img src={item.marketStats} />{" "}
+                    </td>
+                    <td className="crypto-action">
+                      <Button ClASSS={"blue"}>{item.action}</Button>{" "}
+                    </td>
                   </tr>
                 ))}
               </tbody>
---
+++ src/pages/transfer/InternalTransfer.js
@@ -0,0 +1,11 @@
+import { Input } from "../../components/input/Input";
+export const InternalTransfer = () => {
+  return (
+    <div className="">
+      <h1>Internal Transfer</h1>
+      <Input label={'UID'} placeholder={'Enter UID'} name={'UID'}/>
+      <Input label={'UID'} placeholder={'Enter UID'} name={'UID'}/>
+      <Input label={'UID'} placeholder={'Enter UID'} name={'UID'}/>
+    </div>
+  );
+};
